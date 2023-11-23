c5bf4250b3dbe653d8be5aefaeff2c1fc3b037c6
yuri1022 <miu10067@gmail.com>
2023-11-23 11:27:43 +0800

    build: use express-handlebars as view engine

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express')
+const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
 
+app.engine('.hbs', engine({ extname: '.hbs' }))
+app.set('view engine', '.hbs')
+app.set('views', './views')
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-  res.send('this is rereurl page')
+  res.render('index')
 })
+
 
 app.listen(port, () => {
   console.log(`express server is running on http://localhost:${port}`)
